e6b8b3883c27087fbef3efc3b2744695ce15675d
Gr_n RF <gr.nrfinfo@gmail.com>
2022-12-03 10:48:18 +0600

    Added Task_16

---
+++ Task_16.py
@@ -0,0 +1,17 @@
+#  Задача 16. Задайте список из n чисел последовательности (1+ 1n)n и выведите на экран их сумму.
+
+def Posledov(x):
+    sum = 0
+    print(f'Для n={x} ', end='')
+    print('{', end='')
+    for i in range(1, x+1):
+        y = round((1+1*i)*i, 2)
+        if i < x:
+            print(f'{i}: {y}, ', end='')
+        else:
+            print(f'{i}: {y}', end='')
+        sum += y
+    print('}')
+    print(f'Сумма: {sum}')
+Chislo_input = int(input('Введите Ваше любимое число: '))
+Posledov(Chislo_input)
